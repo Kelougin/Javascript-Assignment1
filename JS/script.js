fcaef198413e67051db81bf14a6d68d791ed55da
Jeremy <jeremyrocha@rogers.com>
2025-05-30 10:35:50 -0400

    Javascript noun displayed

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,8 @@ title.textContent = "Story Maker"; //Set title
 //Set nouns
 const nounHolder = document.querySelector('section div:nth-child(1) ol'); //Select ol for nouns
 let nounArray = ["The Turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"]; // Set noun array
-for (let i = 0; i < nounArray.length; i ++){
-    let list = document.createElement("li");
-    nounHolder.append(list);
-    list.textContent = `${nounArray[i]}`;
+for (let i = 0; i < nounArray.length; i ++){ //For loop to cycle through array
+    let list = document.createElement("li"); //Creates li element
+    nounHolder.append(list); //Add li to ol element
+    list.textContent = `${nounArray[i]}`; //Displays noun in li's text content
 }
